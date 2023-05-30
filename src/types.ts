71d28e35ab250875f7f81e0bc42a48e0cf45fdfc
Julia-slvbrg <julia.slvb@gmail.com>
2023-05-30 19:05:01 -0300

    array de produtos e users tipados

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type TUser = {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    createdAt: string  //AA-MM-DD-hh-mm-ss-msms
+}
+
+export type TProduct = { 
+    id: string,
+    name: string,
+    price: number,
+    description: string,
+    imageUrl: string
+}
+
